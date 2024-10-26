352ba9ee11e0a925bd10d12d2f1e0f3ee6501b9b
meetbhuva05 <meetbh05@gmail.com>
2024-10-26 13:57:55 +0530

    changes in about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -7,15 +7,16 @@ const About = () => {
       <h2>About Us</h2>
 
       <p>Free Online Games - Play Now On NajoxGames</p>
-      <p>NajoxGames features the latest and best free online games. We offer instant play to all our games without downloads, login, popups, or other distractions. You can enjoy playing fun games without interruptions from downloads or pop-ups.</p>
-      <p>NajoxGames has the best free online games selection and offers the most fun experience to play alone or with friends.</p>
+      <p>Welcome to NajoxGames, where you’ll find the latest and best selection of free online games. Enjoy instant play with no downloads, logins, or pop-ups—just fun, uninterrupted gaming!</p>
 
       <h2>About Games</h2>
-      <p>Our mission is simple - to create a browser-gaming platform that works seamlessly for users around the world, and rewards developers both big and small.</p>
+      <p>At NajoxGames, our mission is simple: to create a seamless browser-gaming platform that works for users around the world while rewarding developers, both big and small. Our games are designed to be played on desktop, tablet, and mobile devices, so you can enjoy them anywhere—at school, home, or on the go.
+      </p>
       <p>Our games are playable on desktop, tablet, and mobile so you can enjoy them at school, at home, or on the road. Every month millions of gamers from all over the world play their favorite games on NajoxGames. Our goal is to create the ultimate online playground. Free and open to all.</p>
-      <p>We're a team of makers, techies, adventurers – and some gamers too. We’re kids of all ages, and love what we do.</p>
-      <p>Just load up your favorite games instantly in your web browser and enjoy the experience.</p>
-      <p>You can play our games on desktop mobile devices. That includes everything from desktop PCs, laptops, and Chromebooks, to the latest smartphones and tablets from Apple and Android.</p>
+      <p>Every month, millions of gamers from around the globe dive into their favorite games on NajoxGames. Our goal is to create the ultimate online playground, free and open to all.
+      </p>
+      <p>Just load up your favorite games instantly in your web browser and enjoy the fun—no downloads required!
+      </p>
     </div>
   )
 }
